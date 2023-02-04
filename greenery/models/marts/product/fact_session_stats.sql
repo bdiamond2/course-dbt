0fdef06c44daca89169af47f295abb0181f29b67
bdiamond2 <bdiamond2@wisc.edu>
2023-02-04 01:17:55 +0000

    dbt_utils test

--- greenery/models/marts/product/fact_session_stats.sql
+++ greenery/models/marts/product/fact_session_stats.sql
@@ -1,6 +1,6 @@
-{{
-    config(materialized='table')
-}}
+{{ config(materialized='table') }}
+
+{%- set event_types = get_event_types() -%}
 
 with final as (
     select
@@ -9,10 +9,9 @@ with final as (
         , max(e.created_at_utc) as session_end
         , datediff('minute', session_start, session_end) as session_len_minutes
         , count(e.event_guid) as event_count
-        , sum(case when e.event_type = 'checkout' then 1 else 0 end) as checkout_count
-        , sum(case when e.event_type = 'package_shipped' then 1 else 0 end) as package_shipped_count
-        , sum(case when e.event_type = 'add_to_cart' then 1 else 0 end) as add_to_cart_count
-        , sum(case when e.event_type = 'page_view' then 1 else 0 end) as page_view_count
+        {% for etype in event_types %}
+        , sum(case when e.event_type = '{{etype}}' then 1 else 0 end) as {{etype}}_count
+        {% endfor %}
         , sum(case when e.event_type = 'checkout' then fo.order_total else 0 end) as order_total
     from {{ ref('stg_postgres__events') }} e
     left join {{ ref('fact_orders') }} fo on e.order_guid = fo.order_guid
